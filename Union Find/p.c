b57a0a8d682f4413ade6113d3c6b928222bf079e
Aakarsh Sinha <aakarshsak@gmail.com>
2019-03-13 17:47:57 +0530

    Union Find : starting it.....

---
+++ Union Find/p.c
@@ -0,0 +1,72 @@
+/*	
+	Union find algorith to find if a cycle exist or not in a graph
+	Input:A graph in the form  of  a square matrix
+	Output:true if cycle exist otherwise false  	
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define INF 999999
+
+
+//Function to find the vertex in the graph
+//Function to find out if there is a cycle in  the graph or not
+bool isCycle(int g[50][50],int v,int e,int ed[50][50])
+{
+	int parent[v],x,y,i;
+	for(i=0;i<v;i++)
+	{
+		parent[i]=i;
+	}
+
+	for(i=0;i<e;i++)
+	{
+		int src=ed[i][0];
+		int des=ed[i][1];
+		//x=find(parent,src);
+		//y=find(parent,des);
+		if(x==y)
+		{
+			return 1;
+		}
+
+		//union(parent,parent[i]);
+	}
+
+	return 0;
+}
+//main function
+int main()
+{
+	int i,e,v,j,g[50][50];
+
+	printf("\nEnter the number of vertices and edges :\t");
+	scanf("%d%d",&v,&e);
+
+	int edges[e][2];
+	//taking input from the user for the graph
+	int x=0;
+	for(i=0;i<v;i++)
+	{
+		for(j=0;j<v;j++)
+		{
+			scanf("%d",&g[i][j]);
+			if(g[i][j]!=INF && i<j)
+			{
+				edges[x][0]=i;
+				edges[x++][1]=j;
+			}
+		}
+	}
+
+	if(isCycle(g,v,e,edges))
+	{
+		printf("\nCycle exist.....");
+	}
+	else
+	{
+		printf("\nCycle does not exist......");
+	}
+	return 0;
+}
